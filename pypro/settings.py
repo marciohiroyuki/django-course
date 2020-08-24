4822ff9a90f06cf1bdce8cfa5d51f44a9d4f5b2b
Marcio Hiroyuki Miyamoto <marcio.miyamoto@engdb.com.br>
2020-08-24 19:54:37 -0300

    Added migration on heroku.

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,6 +32,7 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # ['django.hirolabs.com.br', 'hirodjangocourse.herokuapp.com']
 
+AUTH_USER_MODEL = 'base.User'
 
 # Application definition
 
